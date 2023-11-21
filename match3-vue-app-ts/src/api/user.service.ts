e7eb1d7118c80d1630fd7242c97328b59e2327cf
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-11-21 15:02:13 +0100

    added api services

---
+++ match3-vue-app-ts/src/api/user.service.ts
@@ -0,0 +1,87 @@
+import { User } from "@/model/user";
+import { Token } from "@/model/token";
+
+export class UserService {
+  static PATH: string;
+
+  private constructor() {
+    UserService.PATH = "http://localhost:9090";
+  }
+
+  static async login(user: User) {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify(user);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow" as RequestRedirect,
+    } as RequestInit;
+
+    return fetch(`${UserService.PATH}/login`, requestOptions);
+  }
+
+  static async register(user: User) {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify(user);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    } as RequestInit;
+
+    return fetch(`${UserService.PATH}/users`, requestOptions);
+  }
+
+  static async getUser(token: Token) {
+    const apiUrl = new URL(`${UserService.PATH}/users/${token.userId}`);
+    if (token) {
+      apiUrl.searchParams.append("token", token.token.token);
+    }
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow" as RequestRedirect,
+    } as RequestInit;
+
+    return fetch(apiUrl.toString(), requestOptions);
+  }
+
+  static async updateUserProfile(
+    token: Token,
+    profileUpdates: { password?: string }
+  ) {
+    const apiUrl = new URL(`${UserService.PATH}/users/${token.userId}`);
+
+    if (token) {
+      apiUrl.searchParams.append("token", token.token.token);
+    }
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestBody = JSON.stringify({
+      password: profileUpdates.password,
+    });
+
+    const requestOptions = {
+      method: "PATCH",
+      headers: myHeaders,
+      body: requestBody,
+      redirect: "follow" as RequestRedirect,
+    } as RequestInit;
+
+    return fetch(apiUrl.toString(), requestOptions);
+  }
+}
