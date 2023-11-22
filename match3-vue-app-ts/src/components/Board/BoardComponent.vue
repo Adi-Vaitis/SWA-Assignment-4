63e3e4d0009e06fb55d2c23771225b2f108371d9
ioanbaicoianu <140078209+ioanbaicoianu@users.noreply.github.com>
2023-11-22 12:28:56 +0100

    added board item

--- match3-vue-app-ts/src/components/Board/BoardComponent.vue
+++ match3-vue-app-ts/src/components/Board/BoardComponent.vue
@@ -5,6 +5,19 @@
       <h3>Current game ID: {{ this.game.gameId }}</h3>
       <h3>Left moves: {leftMoves - not added yet}</h3>
     </div>
+    <div>
+      <div v-for="row in this.game.board.board" :key="row">
+        something
+        <div v-for="col in row" :key="col">
+          SOMETHING
+          <!--            <BoardItem-->
+          <!--              :image="col"-->
+          <!--              :isSelected="selectedPositionToMove.row === rowIndex && selectedPositionToMove.col === colIndex"-->
+          <!--              @click="handleBoardItemClick(rowIndex, colIndex)"-->
+          <!--            />-->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ match3-vue-app-ts/src/components/BoardItem/BoardItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="board-item-container">
+    <img
+      :src="require(`../../assets/BoardImages/${image}`)"
+      :style="{
+        width: '100px',
+        height: '100px',
+        objectFit: 'contain',
+        border: isSelected ? '2px solid yellow' : 'none',
+      }"
+      @click="onClick"
+    />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    // image: string;
+    // isSelected: boolean;
+    // onClick: () => void;
+    image: {
+      type: String,
+      required: true,
+    },
+    isSelected: {
+      type: Boolean,
+      required: true,
+    },
+    onClick: {
+      type: Function,
+      required: true,
+    },
+  },
+  setup(props) {
+    //
+  },
+};
+</script>
